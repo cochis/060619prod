b22e1408e6279262b104de804703b6c25710084b
Oscar Alejandro Ramirez Rosas <ing.oarrs@gmail.com>
2019-06-07 10:33:38 -0500

    base
    base

--- keys.js
+++ keys.js
@@ -5,7 +5,7 @@ exports.default = {
         // host: '108.179.194.74',
         // user: 'ambulanc_inven',
         // password: '?^KW1-cG_2w7',
-        host: '127.0.0.1',
+        host: 'localhost',
         user: 'root',
         password: 'AmbulanciasH2019',
         database: 'ambulanc_newbase'
